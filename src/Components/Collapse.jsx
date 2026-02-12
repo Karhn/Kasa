f318771e90d8f6143fddac81f59ebe6dac826884
Karhn <drkarhn@gmail.com>
2026-02-12 20:51:43 +0100

    Finalisation page About

---
+++ src/Components/Collapse.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Collapse({ title, children }) {
+    const [isOpen, setIsOpen ] = useState(false);
+
+    return (
+        <section className={`collapse ${isOpen ? "isOpen" : ""}`}>
+            <button type="button" className="collapse-header" onClick={() => setIsOpen((value) => !value)}>
+                <span className="collapse-title">{title}</span>
+
+                <span className="collapse-icon">
+                    <svg width="24" height="14" viewBox="0 0 24 14">
+                        <path d="M2 12L12 2L22 12" fill="none" stroke="white" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                </span>
+            </button>
+
+            <div className="collapse-content" hidden={!isOpen}>
+                <div className="collapse-content-inner">
+                    {children}
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,3 +1,40 @@
+import Banner from "../Components/Banner";
+import BannerImg from "../assets/ABOUTBANNER.png"
+
+import Collapse from "../Components/Collapse";
+
+const aboutData = [
+    {
+        title: "Fiabilité",
+        content: "Les annonces postées sur Kasa garantissent une fiabilité totle. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+    },
+    {
+        title: "Respect",
+        content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discrimatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+        title: "Service",
+        content: "La qualité du service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.",
+    },
+    {
+        title: "Sécurité",
+        content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locatoire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+]
+
+
 export default function About() {
-    
+    return (
+        <div className="about">
+            <Banner image={BannerImg} />
+
+            <div className="about-collapse">
+                {aboutData.map((item) => (
+                    <Collapse key={item.title} title={item.title}>
+                        <p>{item.content}</p>
+                    </Collapse>
+                ))}
+            </div>
+        </div>
+    );
 }
